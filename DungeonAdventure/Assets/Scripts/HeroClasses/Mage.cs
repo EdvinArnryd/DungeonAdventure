2e3b1593870a0e5ea4652de4640f9836f7d51380
unknown <edvin.arn@hotmail.com>
2024-10-08 23:52:01 +0200

    Added CharacterCreation scene. Added more hero classes

---
+++ DungeonAdventure/Assets/Scripts/HeroClasses/Mage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mage : HeroClass
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        strength = 2;
+        agility = 4;
+        intelligence = 12;
+        className = "Mage";
+        description = "The wise mage wields the power of the elements to take down his foes.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DungeonAdventure/Assets/Scripts/HeroClasses/Rogue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rogue : HeroClass
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        strength = 6;
+        agility = 10;
+        intelligence = 6;
+        className = "Rogue";
+        description = "The sneaky rogue has a few tricks up his sleave to decieve anyone who crosses him.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
